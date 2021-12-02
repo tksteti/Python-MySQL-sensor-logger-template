ec94a502ca4459e1149e92b75bc2f35434483a4f
KubMakCZ <KubMakCZ@gmail.com>
2021-12-02 15:56:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import mysql.connector
 import random
 from datetime import datetime
 import time
+from gpiozero import CPUTemperature
 
+### Nastavení lokálního serveru
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -15,14 +17,17 @@ mycursor = mydb.cursor()
 sql = "INSERT INTO logger (temp, time) VALUES (%s, %s)" #Základní insert příkaz -> NEMĚNÍ SE
 
 while True:
-    #generování hodnot
-    temperka = random.randint(0,22)
+    #generování/našítání hodnot
+    #temperka = random.randint(0,22) #generovani náhodného čísla
+    cpu = CPUTemperature()
+    print(cpu.temperature)
+
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d %H:%M:%S") #formátování času
     print(dt_string)
 
     #zapisování do databáze
-    val = (temperka, dt_string) #hodnoty které budeme simulovat -> MĚNÍ SE
+    val = (cpu, dt_string) #hodnoty které budeme simulovat -> MĚNÍ SE
     mycursor.execute(sql, val)
     mydb.commit()
     print(mycursor.rowcount, "record inserted.")
